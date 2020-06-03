fb656741818baaf11a353b8cdf712fe0906a43db
A-tG <a3tg.developer@gmail.com>
2020-06-03 14:19:27 +0300

    FIxed bugs, added 32bit support

--- reinstall_driver.ps1
+++ reinstall_driver.ps1
@@ -1,6 +1,11 @@
 $FIRST_DRIVER = 'CorsairVBusDriver.inf'
 $SECOND_DRIVER = 'CorsairVBusDriver.inf'
+$IS_32BIT = ![Environment]::Is64BitOperatingSystem;
 $DEFAULT_DRIVER_PATH = "${Env:ProgramFiles(x86)}\Corsair\CORSAIR iCUE Software\driver\hid"
+if ($IS_32BIT)
+{
+    $DEFAULT_DRIVER_PATH = "${Env:ProgramFiles}\Corsair\CORSAIR iCUE Software\driver\hid"
+}
 
 Function GetOemNameFromOriginal
 {
@@ -9,20 +14,35 @@ Function GetOemNameFromOriginal
         ForEach-Object { ($_.Context.PreContext[0] -split ':\s+')[1] }
 }
 
+$corsairCompositeDevInfo = pnputil /enum-devices |
+    Select-String -Context 2, 4 ('Device Description:\s+' + 'Corsair composite virtual input device')
+$isStopped = $corsairCompositeDevInfo.ToString() | Select-String -Context 1 ('Status:\s+' + 'Stopped')
+$isDisconnected = $corsairCompositeDevInfo.ToString() | Select-String -Context 1 ('Status:\s+' + 'Disconnected')
+If (!($isStopped -or $isDisconnected))
+{
+    Write-Host "No actions needed"
+    exit 0
+}
+
 $driversFolder = $DEFAULT_DRIVER_PATH
-$corsairInstallRegistry = Get-ChildItem HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall |
+$registryInstallPaths = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+if ($IS_32BIT)
+{
+    $registryInstallPaths = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall'
+}
+$corsairInstallRegistry = Get-ChildItem $registryInstallPaths |
     Where-Object {$_.GetValue('DisplayName') -match "ICUE"}
 if ($corsairInstallRegistry)
 {
-    $driversFolder = Join-Path $corsairInstallRegistry.GetValue('InstallLocation') $DRIVER_SUB_PATH
+    $driversFolder = Join-Path $corsairInstallRegistry.GetValue('InstallLocation') 'driver\hid'
 } else
 {
     Write-Host "Unable to find ICUE install location"
     Write-Host "Trying default path"
 }
 
-$fullFirstDriverPath = Join-Path $DEFAULT_DRIVER_PATH $FIRST_DRIVER
-$fullSecondtDriverPath = Join-Path $DEFAULT_DRIVER_PATH $SECOND_DRIVER
+$fullFirstDriverPath = Join-Path $driversFolder $FIRST_DRIVER
+$fullSecondtDriverPath = Join-Path $driversFolder $SECOND_DRIVER
 $isDriversExist = (Test-Path $fullFirstDriverPath) -and (Test-Path $fullSecondtDriverPath)
 if (!($isDriversExist))
 {
@@ -30,18 +50,9 @@ if (!($isDriversExist))
     exit 1
 }
 
-$corsairCompositeDevInfo = pnputil /enum-devices |
-    Select-String -Context 2, 4 ('Device Description:\s+' + 'Corsair composite virtual input device')
-$isStopped = $corsairCompositeDevInfo.ToString() | Select-String -Context 1 ('Status:\s+' + 'Stopped')
-If ($isStopped)
-{
-    Write-Host "Corsair Composite Device is disabled"
-    Write-Host "Reinstalling drivers"
-    pnputil /delete-driver (GetOemNameFromOriginal $FIRST_DRIVER) /uninstall /force
-    pnputil /delete-driver (GetOemNameFromOriginal $SECOND_DRIVER) /uninstall /force
-    pnputil /add-driver (Join-Path $driversFolder CorsairVBusDriver.inf) /install
-    pnputil /add-driver (Join-Path $driversFolder CorsairVHidDriver.inf) /install
-} else 
-{
-    Write-Host "No actions required"
-}
+Write-Host "Corsair Composite Virtual Input Device is disabled"
+Write-Host "Reinstalling drivers"
+pnputil /delete-driver (GetOemNameFromOriginal $FIRST_DRIVER) /uninstall /force
+pnputil /delete-driver (GetOemNameFromOriginal $SECOND_DRIVER) /uninstall /force
+pnputil /add-driver (Join-Path $driversFolder CorsairVBusDriver.inf) /install
+pnputil /add-driver (Join-Path $driversFolder CorsairVHidDriver.inf) /install
